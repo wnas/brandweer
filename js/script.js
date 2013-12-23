379ee0ae6a3e202f5e5ea3a1fa29886f9ddbd83f
wnas <wilfred@voorhoede.nl>
2013-12-23 16:39:43 +0100

    leafletjs also local

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ var brandweer = function () {
         "data":{
             "dummy":{
                 "contact":{
-                    "firstname":"voornaam",
-                    "surname":"achternaam",
-                    "companyname":"bedrijfsnaam",
+                    "firstname":"Milo",
+                    "surname":"van der Linden",
+                    "companyname":"ollyMolly",
                     "address":{
                         "street":"straat naam",
                         "number":"2",
@@ -16,7 +16,7 @@ var brandweer = function () {
                         "municipality":"Den Bosch"
                     },
                     "email":"gerben@mail.ma",
-                    "tel":"+316123456789"
+                    "telephone":"+316123456789"
                 },
                 "buildings":[
                     {
@@ -195,28 +195,31 @@ var brandweer = function () {
 
 
     }, init = function () {
-        // populate();
-        doMaps();
+        populate();
+
+
+        testing();
     }, populate = function () {
         // console.log($.get(config.src));
         $.getJSON(config.src, function (data) {
-            console.log(data.dummy);
+
             var cn = data.dummy.contact.companyname;
             //alert(cn);
         });
-        console.log(config.data.dummy);
+
         var contact = config.data.dummy.contact;
         for (key in contact) {
             if (typeof contact[key] === "object") {
                 var address = config.data.dummy.contact[key];
                 for (i in address) {
-                    console.log(address[i]);
+                    $('#'+i).val(address[i]);
                 }
             } else {
-                console.log(contact[key]);
+                $('#'+key).val(contact[key]);
             }
         }
     }, doMaps = function () {
+        $('#kaart').show();
         var data = {"type":"FeatureCollection", "features":[
             {"type":"Feature", "id":"pand.fid--19c913a3_1431ecdbc7e_-2139", "geometry":{"type":"MultiPolygon", "coordinates":[
                 [
@@ -315,20 +318,28 @@ var brandweer = function () {
         };
 
 
-//        var coords = [51.690599, 5.3064146];
-//       // var map = L.map('map').setView(coords, 18);
-//        var cloudmadeUrl = 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg',
-//            subDomains = ['1', '2', '3', '4'],
-//            cloudmade = new L.TileLayer(cloudmadeUrl, {subdomains:subDomains, maxZoom:18});
-//
-//        map.addLayer(cloudmade);
-//
-//        var marker = L.marker(coords).addTo(map);
-//
-//        marker.bindPopup("<h3>Ha gerben!</h3><p>hier zitten we</p>").openPopup();
-//
-//        L.geoJson(milo).addTo(map);
+        var coords = [51.690599, 5.3064146];
+      var map = L.map('map').setView(coords, 18);
+        var cloudmadeUrl = 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg',
+            subDomains = ['1', '2', '3', '4'],
+            cloudmade = new L.TileLayer(cloudmadeUrl, {subdomains:subDomains, maxZoom:18});
+
+        map.addLayer(cloudmade);
+
+        var marker = L.marker(coords).addTo(map);
+
+        marker.bindPopup("<h3>Ha gerben!</h3><p>hier zitten we</p>").openPopup();
+
+        L.geoJson(milo).addTo(map);
+    },  testing = function(){
+
+        $('#contact button[type=submit]').click(function(){
+            $('fieldset').hide();
+            doMaps();
+
+        })
     };
+
     return {
         init:init
     };
@@ -336,79 +347,4 @@ var brandweer = function () {
 
 brandweer.init();
 
-/*
-
- /**
- * Leaflet biedt standaard geen ondersteuning voor proj4js, de bibliotheek
- * die nodig is voor het omzetten van het transformeren van coordinaat-
- * systemen.
- *
- * Hiervoor heeft Bart van den Eijnden een stukje integratie geschreven.
- * bron: https://github.com/bartvde/PDOK-Leaflet
- */
-
-L.CRS.proj4js = (function () {
-    var createProjection = function (code, def, /*L.Transformation*/ transformation) {
-        if (typeof(def) !== 'undefined') {
-            Proj4js.defs[code] = def;
-        }
-        var proj = new Proj4js.Proj(code);
-
-        return {
-            project: function (latlng) {
-                var point = new L.Point(latlng.lng, latlng.lat);
-                return Proj4js.transform(Proj4js.WGS84, proj, point);
-            },
-
-            unproject: function (point, unbounded) {
-                var point2 = Proj4js.transform(proj, Proj4js.WGS84, point.clone());
-                return new L.LatLng(point2.y, point2.x, unbounded);
-            }
-        };
-    };
-
-    return function (code, def, transformation) {
-        return L.Util.extend({}, L.CRS, {
-            code: code,
-            transformation: transformation ? transformation: new L.Transformation(1, 0, -1, 0),
-            projection: createProjection(code, def)
-        });
-    };
-}());
-
-// maak een array met de resoluties benodigd voor het tileschema:
-var rd_res = [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420];
-// maak van de projectiestring voor het Rijksdriehoekstelsel een Leaflet projectie object:
-var rd = L.CRS.proj4js('EPSG:28992',
-    '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 ' +
-        '+ellps=bessel +units=m ' +
-        '+towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs',
-    new L.Transformation(
-        1,
-        285401.920,
-        -1,
-        903401.920
-    ));
-// Override de scale instelling voor het schema:
-rd.scale = function(zoom) {
-    return 1 / rd_res[zoom];
-};
-// Kaart initialiseren:
-var map = new L.Map('map', {
-    continuousWorld: true,
-    crs: rd,
-    layers: [
-        // De layer van de openbasiskaart
-        new L.TileLayer(
-            'http://www.openbasiskaart.nl/mapcache/tms/1.0.0/osm@rd/{z}/{x}/{y}.png', {
-                tms: true,
-                minZoom: 3,
-                maxZoom: 13,
-                continuousWorld: true
-            }
-        )
-    ],
-    center: new L.LatLng(52, 5.3),
-    zoom: 3
-});
 
