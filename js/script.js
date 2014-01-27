0933e5d3d8ae454b065b9aba504d40b1943dfb49
Gerben Hoeve <gerben@mac.com>
2014-01-27 11:26:13 +0100

    leaflet css

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ var brandweer = function () {
             "answers":[],
             "active":"active",
             "numberOfQuestions":16,
-            "tmpl_dir":'/templates',
+            "tmpl_dir":'/Brandweer/templates',
             "mainNavigation":$('.top-navigation'),
             "info":{
                 "show":".revealInformation",
